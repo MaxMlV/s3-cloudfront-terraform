e949621830e21ae473557818e9902bada1452967
MaxMlV <max.mlv.k@gmail.com>
2022-07-17 19:19:37 +0300

    dev branch

--- main.tf
+++ main.tf
@@ -7,3 +7,19 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "null_resource" "build" {
+  provisioner "local-exec" {
+    command = "cd my-app && npm install && ./node_modules/.bin/gatsby build"
+  }
+
+  depends_on = [aws_s3_bucket.website_bucket]
+}
+
+resource "null_resource" "deploy" {
+  provisioner "local-exec" {
+    command = "aws s3 sync my-app/public/ s3://${var.bucket_name}"
+  }
+
+  depends_on = [null_resource.build]
+}
+
